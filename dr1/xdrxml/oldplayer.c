7d9d09200bd36dab8c4e004d4b1491cb4b360104
ksmathers <>
2001-04-24 05:04:55 +0000

    Some documentation and test code for xdrxml.

---
+++ dr1/xdrxml/oldplayer.c
@@ -0,0 +1,62 @@
+#include <rpc/xdr.h>
+#include "xdrxml.h"
+
+struct Location {
+    int x;
+    int y;
+};
+
+struct Player {
+    char *name;
+    struct Location loc;
+};
+
+bool_t xdr_Location( XDR *xdrs, struct Location *l) {
+    xdr_attr( xdrs, "x");
+    if (!xdr_int( xdrs, &l->x)) return FALSE;
+
+    xdr_attr( xdrs, "y");
+    if (!xdr_int( xdrs, &l->y)) return FALSE;
+    return TRUE;
+}
+
+bool_t xdr_Player( XDR *xdrs, struct Player *p) {
+    xdr_attr( xdrs, "name");
+    if (!xdrxml_wrapstring( xdrs, &p->name)) return FALSE;
+
+    xdr_push_note( xdrs, "loc");
+    if (!xdr_Location( xdrs, &p->loc)) return FALSE;
+    xdr_pop_note( xdrs);
+    return TRUE;
+}
+
+int main( int argc, char **argv) {
+    XDR xdrs; 
+    struct Player bob = { "Bob", { 10, 49 }};
+    int op;
+
+    if (argc != 2) {
+	printf("Usage: newplayer (read|write)\n");
+	exit(1);
+    }
+
+    if (!strcmp( argv[1], "read")) {
+	op = XDR_DECODE;
+    } else {
+	op = XDR_ENCODE;
+    }
+
+    xdr_xml_create( &xdrs, "player.xml", op);
+    xdr_push_note( &xdrs, "bob");
+    xdr_Player( &xdrs, &bob);
+    xdr_pop_note( &xdrs);
+    xdr_destroy( &xdrs); 	
+
+    if (op == XDR_DECODE) {
+	printf("  Player: %s\n", bob.name);
+	printf("Location: (%d,%d)\n", bob.loc.x, bob.loc.y);
+    }
+    return 0;
+}
+
+
