9af74134a1cef7b3efd25cc92e5469028e893dd4
ksmathers <>
2002-08-02 19:08:19 +0000

    Added header to remove some undefined functions

---
+++ dr1/sdlclient/cgenerate.h
@@ -0,0 +1,12 @@
+#ifndef __CGENERATE__H
+#define __CGENERATE__H
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+
+#include "player.h"
+
+void
+cgenerate_setPlayer( dr1Player *p); 
+
+#endif /* __CGENERATE__H */
+
